133a7ecb1938594ae51464f07b2be3ee0be1f3ad
hansol-FE <dundunki@naver.com>
2022-05-05 13:40:57 +0900

    [modify] 로그인 Form 수정

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,6 +1,35 @@
+import {useState} from "react"
+
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const onChange = (e) => {
+    const{ target : {name,value} } = e;
+    
+    if(name === "email"){
+      setEmail(value);
+    }else if(name === "password"){
+      setPassword(value);
+    }
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return(
-    <span>Auth</span>
+   <div>
+     <form onSubmit={onSubmit}>
+       <input type="email" name="email" placeholder="Email" value={email} onChange={onChange} required />
+       <input type="password" name="password" placeholder="Password" value={password} onChange={onChange} required />
+       <input type="submit" value="Log In" />
+     </form>
+     <div>
+       <button>Continue with Google</button>
+       <button>Continue with Github</button>
+     </div>
+   </div>
   )
 }
 
