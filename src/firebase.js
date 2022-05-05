47c2fea97275d2aea234181ba45ea00126d602ce
hansol-FE <dundunki@naver.com>
2022-05-05 11:43:01 +0900

    [add] firebase 세팅

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAIzDTWZZ8K5rhbPs34rVSOOsF_8Bg2j2w",
+  authDomain: "twitter-76998.firebaseapp.com",
+  projectId: "twitter-76998",
+  storageBucket: "twitter-76998.appspot.com",
+  messagingSenderId: "348593123290",
+  appId: "1:348593123290:web:f5711db8c885f65db7dae3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
