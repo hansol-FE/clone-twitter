9782c066c7acc8da4969d41e0c1afa113f1586ff
hansol-FE <dundunki@naver.com>
2022-05-05 14:16:05 +0900

    [modify] firebase 이용한 로그인 로직 추가

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import AppRouter from 'components/Router'
 
 import { authService } from "fbase"
 
 function App() {
+  const [init, setInit] = useState(false);
   const [isLoggedIn, setLoggedIn] = useState(authService.currentUser);
+  
+  useEffect(()=>{
+    authService.onAuthStateChanged((user) => {
+      if(user){
+        setLoggedIn(user);
+      }else{
+        setLoggedIn(false);
+      }
+      setInit(true);
+    });
+  },[])
   return (
   <>
-    <AppRouter isLoggedIn={isLoggedIn} />
+    {init ? <AppRouter isLoggedIn={isLoggedIn} /> : "initializing..." }
     <footer>&copy; {new Date().getFullYear()} twitter</footer>
   </>
   )
